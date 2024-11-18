738cf4ef16590478ad21f1ce3fa32451f647f519
Emil K <29043737+Cosmao@users.noreply.github.com>
2024-11-18 10:34:32 +0100

    Surely kek

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -7,9 +7,8 @@ specifies that any user authenticated via an API key can "create", "read",
 "update", and "delete" any "Todo" records.
 =========================================================================*/
 const schema = a.schema({
-  Todo: a
-    .model({
-      content: a.string(),
+  Todo: a.model({
+    content: a.string(),
   }).authorization(allow => [allow.owner()]),
 });
 
