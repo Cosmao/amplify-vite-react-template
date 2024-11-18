625e764e75dc086b88b1a93573b50534008706a8
Emil K <29043737+Cosmao@users.noreply.github.com>
2024-11-18 13:01:26 +0100

    added the echo handler

---
+++ amplify/data/echo-handler/handler.ts
@@ -0,0 +1,19 @@
+import type { Schema } from '../resource'
+
+export const handler: Schema["addTelemetry"]["functionHandler"] = async (event, context) => {
+    console.log(`Echoing content: ${JSON.stringify(event)}`)
+
+    const res = {
+        device_id: event.arguments.device_id,
+        timestamp: event.arguments.timestamp,
+        owner: event.arguments.owner,
+        temperature: event.arguments.temperature,
+        humidity: event.arguments.humidity,
+        createdAt: new Date(event.arguments.timestamp).toISOString(),
+        updatedAt: new Date(event.arguments.timestamp).toISOString(),
+    };
+
+    console.log("res: ", res);
+
+    return res;
+};
